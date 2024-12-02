494d8c13bd1dd4572dbd72393dbf91a63c597a7a
Suomi228 <bixep345@gmail.com>
2024-12-02 12:05:32 +0300

    фильтр по классу машины

--- src/main/java/org/example/carsharingcontracts/controllers/UserController.java
+++ src/main/java/org/example/carsharingcontracts/controllers/UserController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/user")
 public interface UserController {
-    @GetMapping("/carList")
-    String getCars();
+    @GetMapping("/homePage")
+    String homePage(@RequestParam(value = "carClass", required = false) String carClass, Model model);
     @GetMapping("/carClass")
     String getCarsByClass(@RequestParam String carClass);
     @PostMapping("/rent")
