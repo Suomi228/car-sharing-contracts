58c8de2e3589c573d48480b822a07d44f6a73faf
Suomi228 <bixep345@gmail.com>
2024-12-12 19:33:16 +0300

    fix: перенос авторизации в authController

--- src/main/java/org/example/carsharingcontracts/controllers/AuthController.java
+++ src/main/java/org/example/carsharingcontracts/controllers/AuthController.java
@@ -1,18 +1,26 @@
 package org.example.carsharingcontracts.controllers;
 
-import ch.qos.logback.core.model.Model;
+import jakarta.validation.Valid;
 import org.example.carsharingcontracts.input.LoginInputModel;
 import org.example.carsharingcontracts.input.SignupInputModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/auth")
 public interface AuthController {
-    @PostMapping("/login")
-    void login(@ModelAttribute("input") LoginInputModel input, Model model);
-    @PostMapping("/signup")
-    void signup(@ModelAttribute("input") SignupInputModel input, Model model);
+    @GetMapping("/login")
+    String login();
+    @PostMapping("/login-error")
+    String onFailedLogin(@ModelAttribute("number") String number, RedirectAttributes redirectAttributes);
+    @PostMapping("/register")
+    String register(@ModelAttribute @Valid SignupInputModel signupInputModel, BindingResult bindingResult);
+    @GetMapping("/register")
+    String showRegisterForm(Model model);
 }
