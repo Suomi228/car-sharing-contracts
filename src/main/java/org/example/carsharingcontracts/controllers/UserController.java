3c6616a47949df11a7a44003146c279a52b23e07
Suomi228 <bixep345@gmail.com>
2024-12-12 14:38:51 +0300

    Логин регистрация переделан аренда тачки

--- src/main/java/org/example/carsharingcontracts/controllers/UserController.java
+++ src/main/java/org/example/carsharingcontracts/controllers/UserController.java
@@ -6,13 +6,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/user")
 public interface UserController {
     @GetMapping("/homePage")
     String homePage(@RequestParam(value = "carClass", required = false) String carClass, Model model);
     @PostMapping("/rentCar")
-    String rentCar(@RequestParam Long customerId, @RequestParam Long carId, RedirectAttributes redirectAttributes);
+    String rentCar(Principal principal, @RequestParam Long carId, RedirectAttributes redirectAttributes);
     @GetMapping("/{id}")
     String getMyTrips(@PathVariable Long id, Model model);
     @GetMapping("/returnCarList")
--- src/main/java/org/example/carsharingcontracts/input/SignupInputModel.java
+++ src/main/java/org/example/carsharingcontracts/input/SignupInputModel.java
@@ -14,4 +14,46 @@ public class SignupInputModel {
 
     @NotBlank(message = "Введите пароль")
     String password;
+
+    public SignupInputModel() {
+    }
+
+    public SignupInputModel(String firstName, String lastName, String number, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+        this.password = password;
+    }
+
+    public @NotBlank(message = "Введите имя") String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@NotBlank(message = "Введите имя") String firstName) {
+        this.firstName = firstName;
+    }
+
+    public @NotBlank(message = "Введите фамилию") String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NotBlank(message = "Введите фамилию") String lastName) {
+        this.lastName = lastName;
+    }
+
+    public @NotBlank(message = "Введите номер телефона") String getNumber() {
+        return number;
+    }
+
+    public void setNumber(@NotBlank(message = "Введите номер телефона") String number) {
+        this.number = number;
+    }
+
+    public @NotBlank(message = "Введите пароль") String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotBlank(message = "Введите пароль") String password) {
+        this.password = password;
+    }
 }
