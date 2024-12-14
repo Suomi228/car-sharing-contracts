aa1a018acb7e5048604cf8eb362621651160e311
Suomi228 <bixep345@gmail.com>
2024-12-14 15:31:53 +0300

    рефайктор создания машины и carService

--- src/main/java/org/example/carsharingcontracts/controllers/AdminController.java
+++ src/main/java/org/example/carsharingcontracts/controllers/AdminController.java
@@ -11,8 +11,13 @@ public interface AdminController {
     String getAllCars(@RequestParam(value = "carClass", required = false) String carClass, @RequestParam(value = "carStatus", required = false) String carStatus, Model model);
     @GetMapping("/editCar/{id}")
     String editCar(@PathVariable("id") Long id, Model model);
+    @PostMapping("/create")
+    String createCar(@ModelAttribute CarInputModel carInputModel);
+    @GetMapping("/create")
+    String showCreateCarForm(Model model);
     @PostMapping("/updateCar")
     String updateCar(@ModelAttribute CarInputModel carInputModel);
     @PostMapping("/deleteCar/{id}")
     String deleteCar(@PathVariable("id") Long id);
+
 }
